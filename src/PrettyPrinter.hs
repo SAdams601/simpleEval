ff006a66101309aafa0983d4dd76c6731f983305
Stephen Adams <sa597@kent.ac.uk>
2015-01-29 12:58:24 +0000

    Added state monad pretty printer

--- src/PrettyPrinter.hs
+++ src/PrettyPrinter.hs
@@ -7,7 +7,7 @@ data Storage = Storage {
   expr :: Expr
   
                                  } deriving Show
-type PrintState = State Storage
+type PrintState = State Int
 
 indent = "   "
 
@@ -16,21 +16,57 @@ getIndentLevel l = foldr (++) "" $ take l $ repeat indent
 {-
 
 -}
-prettyPrependPrint :: Storage -> String
-prettyPrependPrint Storage {iState=i, expr=(Var c)} = (getIndentLevel i) ++ [c]
-prettyPrependPrint Storage {iState=i, expr=(N n)} = (getIndentLevel i) ++ show n
-prettyPrependPrint Storage {iState=i, expr=(Assign c e)} =
-  (getIndentLevel i) ++ "(" ++ "<" ++ [c] ++ ":" ++ prettyExpr ++ ")"
-    where prettyExpr = prettyPrependPrint $ Storage {iState=iLevel, expr=e}
+
+statePrettyPrint :: Expr -> PrintState String
+statePrettyPrint (Var c) = do
+  i <- get
+  return $ (getIndentLevel i) ++ [c]
+statePrettyPrint (N n) = do
+  i <- get
+  return $ (getIndentLevel i) ++ show n
+statePrettyPrint (Assign c e) = do
+  i <- get
+  let iLevel = findIndent i e
+      newLine = case iLevel of
+        0 -> ""
+        _ -> "\n"
+  put iLevel
+  prettyExpr <- statePrettyPrint e
+  put i
+  return $ (getIndentLevel i) ++ "<" ++ [c] ++ ":" ++ newLine ++ prettyExpr
+statePrettyPrint (Add e1 e2) = do
+  i <- get
+  put (i+1)
+  prettyE1 <- statePrettyPrint e1
+  prettyE2 <- statePrettyPrint e2
+  put i
+  return $ (getIndentLevel i) ++ "(\n" ++ prettyE1 ++ "\n" ++ (getIndentLevel (i+1))  ++ "+\n" ++ prettyE2 ++ ")"
+statePrettyPrint (Sub e1 e2) = do
+  i <- get
+  put (i+1)
+  prettyE1 <- statePrettyPrint e1
+  prettyE2 <- statePrettyPrint e2
+  put i
+  return $ (getIndentLevel i) ++ "(\n" ++ prettyE1 ++ "\n" ++ (getIndentLevel (i+1))  ++ "-\n" ++ prettyE2 ++ ")"
+
+prettyPrint :: Storage -> String
+prettyPrint Storage {iState=i, expr=(Var c)} = (getIndentLevel i) ++ [c]
+prettyPrint Storage {iState=i, expr=(N n)} = (getIndentLevel i) ++ show n
+prettyPrint Storage {iState=i, expr=(Assign c e)} =
+  (getIndentLevel i) ++ "<" ++ [c] ++ ":" ++ newLine ++ prettyExpr
+    where prettyExpr = prettyPrint $ Storage {iState=iLevel, expr=e}
           iLevel = findIndent i e
-prettyPrependPrint Storage {iState=i, expr=(Add e1 e2)} =
-  (getIndentLevel i) ++ "(" ++ prettyE1 ++ "+\n" ++ prettyE2 ++ ")"
-  where prettyE1 = prettyPrependPrint $ Storage {iState=(i+1), expr=e1}
-        prettyE2 = prettyPrependPrint $ Storage {iState=(i+1), expr=e2}
-prettyPrependPrint Storage {iState=i, expr=(Sub e1 e2)} =
-  (getIndentLevel i) ++ "(\n" ++ prettyE1 ++ "-\n" ++ prettyE2 ++ ")"
-  where prettyE1 = prettyPrependPrint $ Storage {iState=(i+1), expr=e1}
-        prettyE2 = prettyPrependPrint $ Storage {iState=(i+1), expr=e2}  
+          newLine = case iLevel of
+            0 -> ""
+            _ -> "\n"
+prettyPrint Storage {iState=i, expr=(Add e1 e2)} =
+  (getIndentLevel i) ++ "(\n" ++ prettyE1 ++ "\n" ++ (getIndentLevel (i+1))  ++ "+\n" ++ prettyE2 ++ ")"
+  where prettyE1 = prettyPrint $ Storage {iState=(i+1), expr=e1}
+        prettyE2 = prettyPrint $ Storage {iState=(i+1), expr=e2}
+prettyPrint Storage {iState=i, expr=(Sub e1 e2)} =
+  (getIndentLevel i) ++ "(\n" ++ prettyE1 ++ "\n" ++ (getIndentLevel (i+1)) ++ "-\n" ++ prettyE2 ++ ")"
+  where prettyE1 = prettyPrint $ Storage {iState=(i+1), expr=e1}
+        prettyE2 = prettyPrint $ Storage {iState=(i+1), expr=e2}  
 
 findIndent :: Int -> Expr -> Int
 findIndent _ (Var _) = 0
